f76a5f1c00a5bf1f0fadd262497f328c76088b90
Sandra Jasionowska <sandra.jasionowska@boombit.com>
2021-04-24 10:04:38 +0200

    Camera follows player. Added collidable and non-collidable tilemaps that behave well.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,111 @@
+﻿using System.Linq;
+
+using UnityEngine;
+
+/// <summary>
+/// Class responsible for camera following the Player. 
+/// </summary>
+public class CameraController : MonoBehaviour
+{
+	/// <summary>
+	/// Time (in seconds) in which camera will move between two points declared in Vector2.SmoothDamp.
+	/// Changeable from inspector level.
+	/// For more info see: https://docs.unity3d.com/ScriptReference/Vector2.SmoothDamp.html
+	/// </summary>
+	[SerializeField]
+	private float distanceTime = 0.1f;
+
+	/// <summary>
+	/// Default distance between the camera and the player. Changeable from inspector level.
+	/// </summary>
+	[SerializeField]
+	private Vector3 defaultDistanceFromTarget = new Vector3(0f, 2.8f, -4.3f);
+
+	/// <summary>
+	/// Initial velocity. This value is modified every frame, according to the distance the
+	/// camera has to travel and the time defined by distanceTime.
+	/// </summary>
+	private Vector2 velocityVector2D = Vector2.one;
+
+	/// <summary>
+	/// Transform of target followed by Camera during the game. In this case Player is the target.
+	/// </summary>
+	private Transform target;
+
+	/// <summary>
+	/// Camera transform field. 
+	/// </summary>
+	private Transform cameraTransform;
+
+	/// <summary>
+	/// Gets Camera's transform to cameraTransform variable.
+	/// Assigns Player's transform as Camera's target to follow.
+	/// </summary>
+	private void Awake()
+	{
+		cameraTransform = transform;
+		var targets = FindObjectsOfType<PlayerMovement>();
+		if (targets == null || targets.Length == 0) 
+			Debug.LogError("There's no PlayerMovement objects on the scene.");
+		else
+		{
+			if (targets.Length > 1) 
+				Debug.LogError("There's more than one PlayerMovement on the scene.");
+			target = targets.FirstOrDefault()?.gameObject.transform;
+		}
+	}
+
+	/// <summary>
+	/// LateUpdate() is called after all Update functions have been called.
+	/// Camera uses MixedFollow() method to follow Player after he updates his position.
+	/// </summary>
+	private void LateUpdate()
+	{
+		MixedFollow();
+	}
+
+	/// <summary>
+	/// Camera gets target's position and counts the distance 
+	/// defined in defaultDistanceFromTarget Vector3. Going along with it, following the Player is
+	/// divided into two parts: moving along (x,y) axis, using Vector2.SmoothDamp and moving along
+	/// z-axes, simply setting the distance between Camera and Player (defined before in
+	/// "defaultDistanceFromTarget").
+	/// Second part is Camera moves along z-axis simply subtracting default distance, defined before
+	/// in "defaultDistanceFromTarget" Vector3. 
+	/// </summary>
+	private void MixedFollow()
+	{
+		//Vector2 toPosXY represents (x,y) position that Camera is trying
+		// to reach. This Vector2 is created by getting Player's (x,y) position and
+		// adding default (x,y) distance to use it later.
+		var targetPosition = target.position;
+		var toPosXY = new Vector2(
+			targetPosition.x + defaultDistanceFromTarget.x,
+			targetPosition.y + defaultDistanceFromTarget.y);
+
+		//Vector2 "curPos" is a vector that represents desired(x, y) Camera position,
+		// counted by Vector2.SmoothDamp, that takes present Camera (x,y) position as current position and
+		// "toPosXY" as goal position. This method refers to "velocityVector2D" as a field to manipulate speed.
+		// distanceTime, set before, is time (in seconds) in which Camera will move between it's 
+		// current and desired position. Referring to the documentation, "Mathf.Infinity" is maximum
+		// allowed Camera's speed and "Time.deltaTime" is the time since the last call to this function.
+		// This is default value for Vector2.SmoothDamp.
+		var cameraPosition = cameraTransform.position;
+		var curPos = Vector2.SmoothDamp(
+			new Vector2(cameraPosition.x, cameraPosition.y),
+			toPosXY,
+			ref velocityVector2D,
+			distanceTime,
+			Mathf.Infinity,
+			Time.deltaTime);
+
+		// Summarizing, on the x and y axes the desired position is set using values calculated
+		// by Vector2.SmoothDamp method and on the z-axis the desired position is set using subtraction.
+		// At the end, desired position is assigned to cameraTransform field.
+		cameraPosition = new Vector3(
+			curPos.x,
+			curPos.y,
+			targetPosition.z + defaultDistanceFromTarget.z);
+		cameraTransform.position = cameraPosition;
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+
+using UnityEngine;
+
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+[RequireComponent(typeof(Rigidbody2D))]
+// [RequireComponent(typeof(Entity))]
+// [RequireComponent(typeof(PlayerShooting))]
+// [RequireComponent(typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField]
+	private float walkingSpeed = 2f;
+
+	[SerializeField]
+	private float runningSpeed = 3f;
+
+	public float moveSpeed = 5f;
+
+	private Animator animator;
+
+	// private Entity entity;
+
+#pragma warning disable 108,114
+
+	// ReSharper disable once IdentifierTypo
+	public Rigidbody2D rigidbody;
+#pragma warning restore 108,114
+
+	private Vector2 movement;
+
+	// private float xDirection;
+	//
+	// private float yDirection;
+
+	// private GameManager gameManager;
+
+	// public Vector2 Movement
+	// {
+	// 	get => movement;
+	// }
+	//
+	// public Vector2 Direction
+	// {
+	// 	get { return new Vector2(xDirection, yDirection); }
+	// }
+
+	private float currentSpeed;
+
+	// private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+	//
+	// private static readonly int Vertical = Animator.StringToHash("Vertical");
+	//
+	// private static readonly int Speed = Animator.StringToHash("Speed");
+	//
+	// private static readonly int XDirection = Animator.StringToHash("xDirection");
+	//
+	// private static readonly int YDirection = Animator.StringToHash("yDirection");
+
+	private void Start()
+	{
+		// rigidbody = GetComponent<Rigidbody2D>();
+		// animator = GetComponent<Animator>();
+		// entity = GetComponent<Entity>();
+
+		// gameManager = FindObjectOfType<GameManager>();
+	}
+
+	private void Update()
+	{
+		movement.x = Input.GetAxisRaw("Horizontal");
+		// Debug.Log(movement.x);
+		movement.y = Input.GetAxisRaw("Vertical");
+		// Debug.Log(movement.y);
+
+		// currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runningSpeed : walkingSpeed;
+
+		// var movementNormalized = movement;
+		//
+		// animator.speed = currentSpeed / 3;
+		// animator.SetFloat(Horizontal, movementNormalized.x);
+		// animator.SetFloat(Vertical, movementNormalized.y);
+		//
+		// animator.SetFloat(Speed, movement.sqrMagnitude);
+
+		if (Input.GetKeyUp(KeyCode.U))
+		{
+			var position = rigidbody.position;
+			// rigidbody.MovePosition(new Vector2(position.x, position.y + 1.5f));
+			rigidbody.position = new Vector2(position.x, position.y + 3.5f);
+		}		
+		
+		if (Input.GetKeyUp(KeyCode.P))
+		{
+			var position = rigidbody.position;
+			// rigidbody.MovePosition(new Vector2(position.x, position.y - 1.5f));
+			rigidbody.position = new Vector2(position.x, position.y - 3.5f);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		// rigidbody.MovePosition(rigidbody.position + movement * (currentSpeed * Time.fixedDeltaTime));
+		rigidbody.MovePosition(rigidbody.position + movement * (moveSpeed * Time.fixedDeltaTime));
+
+		// if (Math.Abs(movement.magnitude) > 0.5)
+		// {
+		// 	xDirection = animator.GetFloat(Horizontal);
+		// 	yDirection = animator.GetFloat(Vertical);
+		//
+		// 	animator.SetFloat(XDirection, xDirection);
+		// 	animator.SetFloat(YDirection, yDirection);
+		// }
+	}
+
+	// private void OnCollisionEnter2D(Collision2D other)
+	// {
+	// 	if (other.gameObject.CompareTag("ScreamBullet"))
+	// 	{
+	// 		entity.GetHurt();
+	// 	}
+	// }
+
+	// private void OnCollisionStay2D(Collision2D other)
+	// {
+	// 	if (other.gameObject.CompareTag("CoffeeShop"))
+	// 		if (Input.GetKeyDown(KeyCode.E))
+	// 			if (gameManager.GameWon)
+	// 			{
+	// 				WinGame();
+	// 			}
+	// }
+
+	private void WinGame()
+	{
+		// canvas.GetComponent<EndMenu>().ShowEndMenu();
+		// coffeeCanvas.SetActive(false);
+	}
+
+	private void Move()
+	{
+		var walkingDirection = Vector3.zero;
+	
+		walkingDirection += Vector3.up * Input.GetAxis("Vertical");
+		walkingDirection += Vector3.right * Input.GetAxis("Horizontal");
+	
+		walkingDirection = walkingDirection.normalized;
+		walkingDirection *= Input.GetKey(KeyCode.LeftShift) ? runningSpeed : walkingSpeed;
+	
+		rigidbody.MovePosition(transform.position + walkingDirection * Time.fixedDeltaTime);
+	}
+}

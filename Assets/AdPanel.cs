3f1f2af14a74f1b8ada5403208bd6add724d0bb3
Sandra Jasionowska <sandra.jasionowska@boombit.com>
2021-04-24 18:35:00 +0200

    for my fiance

---
+++ Assets/AdPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class AdPanel : MonoBehaviour
+{
+	private bool hidden;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		StartCoroutine(Fade());
+	}
+
+	IEnumerator Fade()
+	{
+		while (!hidden)
+		{
+			yield return new WaitForSeconds(5f);
+
+			gameObject.SetActive(false);
+			hidden = true;
+		}
+	}
+}
--- Assets/Box.cs
+++ Assets/Box.cs
@@ -2,28 +2,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Box : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{   
+    [SerializeField]
+    private GameObject uiText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject uiPanel;
+
+    [SerializeField]
+    private GameObject winPanel;
+
+    private bool withCollistion;
+    
+    private bool winPanelHidden;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log(other.collider.tag);
+        if (other.collider.CompareTag("Player"))
+        {
+            uiPanel.SetActive(true);
+            uiPanel.SetActive(true);
+
+            withCollistion = true;
+        }
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionExit2D(Collision2D other)
     {
-        
+        if (other.collider.CompareTag("Player"))
+        {
+            uiPanel.SetActive(false);
+            uiPanel.SetActive(false);
+
+            withCollistion = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || withCollistion)
+        {
+            winPanel.SetActive(true);
+            StartCoroutine(HideWinPanel());
+        }
+    }
+
+    private IEnumerator HideWinPanel()
+    {
+        while (!winPanelHidden)
+        {
+            yield return new WaitForSeconds(5f);
+
+            winPanel.SetActive(false);
+            winPanelHidden = true;
+            SceneManager.LoadScene("Menu");
+        }
     }
 }

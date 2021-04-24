7d18a2a881521b12885d4053ae3fa1a5252a7164
Sandra Jasionowska <sandra.jasionowska@boombit.com>
2021-04-24 11:49:15 +0200

    Changed background, added movement, added box as destination.

---
+++ Assets/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.collider.tag);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+}
---
+++ Assets/CratesMovement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CratesMovement : MonoBehaviour
+{
+	//Assign a GameObject in the Inspector to rotate around
+	private GameObject target;
+
+	private bool rotatable;
+
+	private void Awake()
+	{
+		target = GameObject.FindGameObjectWithTag("Player");
+		rotatable = true;
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A) && rotatable)
+		{
+			rotatable = false;
+			RotateLeft();
+		}
+
+		if (Input.GetKeyDown(KeyCode.D) && rotatable)
+		{
+			rotatable = false;
+			RotateRight();
+		}
+	}
+
+	private void RotateLeft()
+	{
+		transform.RotateAround(
+			target.transform.position,
+			Vector3.forward,
+			45);
+		rotatable = true;
+	}
+
+	private void RotateRight()
+	{
+		transform.RotateAround(
+			target.transform.position,
+			Vector3.forward,
+			-45);
+		rotatable = true;
+	}
+
+}
